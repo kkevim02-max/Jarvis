589793969d41a4b4215832e9abba41f68a6d3e81
kkevim02-max <kkevem02@gmail.com>
2026-08-19 08:21:09 -0300

    Jarvis 2.0: network protocol v2

--- android/app/src/main/java/com/levy/jarvis/JarvisConfig.java
+++ android/app/src/main/java/com/levy/jarvis/JarvisConfig.java
@@ -4,7 +4,7 @@ public final class JarvisConfig {
     public static final String SECRET = "__JARVIS_SECRET__";
     public static final int HTTP_PORT = 8765;
     public static final int DISCOVERY_PORT = 47665;
-    public static final String DISCOVERY_REQUEST = "JARVIS_DISCOVER_V1";
-    public static final String DISCOVERY_RESPONSE = "JARVIS_CORE_V1";
+    public static final String DISCOVERY_REQUEST = "JARVIS_DISCOVER_V2";
+    public static final String DISCOVERY_RESPONSE = "JARVIS_CORE_V2";
     private JarvisConfig() {}
 }
